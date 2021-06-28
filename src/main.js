1769695ef7fbca2a563d20b822e1511f65c92de9
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-28 16:27:45 -0700

    UI interface partially completed
    Co-Authored-By: Godfrey Owidi<godfreyowiidi@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,28 +4,56 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
+  $('#location-form').submit(function(event) {
+    
+    const inputtedParameter = $("#weather-parameter option:selected").val();
+    event.preventDefault();
+    
+    console.log(inputtedParameter);
+  
+    let choice = $('#location').val();
+    console.log(choice);
 
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
+    function result() {
+      if (inputtedParameter === "lat-lon") {
+        return `http://api.openweathermap.org/data/2.5/weather?${inputtedParameter}=${choice}&appid=${process.env.API_KEY}`;
+      } else if (inputtedParameter === "id") {
+        return `http://api.openweathermap.org/data/2.5/weather?${inputtedParameter}=${choice}&appid=${process.env.API_KEY}`;
+        
+      } else if (inputtedParameter === "q") {
+        return`http://api.openweathermap.org/data/2.5/weather?${inputtedParameter}=${choice}&appid=${process.env.API_KEY}`;
+         
+      } else if (inputtedParameter === "zip") {
+        return `http://api.openweathermap.org/data/2.5/weather?${inputtedParameter}=${choice}&appid=${process.env.API_KEY}`;
+        
+      } else {
+        return "Try again!"
       }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-      $('.showPressure').text(`The pressure is ${response.main.pressure} inHg²`);
-      $('.showClouds').text(`The cloud coverage is: ${response.weather[0].description}`);
+  
     }
+    console.log(result());
+    
+
+
+    //
+
+    //let request = new XMLHttpRequest();
+ 
+    // request.onreadystatechange = function() {
+    //   if (this.readyState === 4 && this.status === 200) {
+    //     const response = JSON.parse(this.responseText);
+    //     getElements(response);
+    //   }
+    // };
+
+    // request.open("GET", url, true);
+    // request.send();
+
+    // function getElements(response) {
+    //   $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+    //   $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    //   $('.showPressure').text(`The pressure is ${response.main.pressure} inHg²`);
+    //   $('.showClouds').text(`The cloud coverage is: ${response.weather[0].description}`);
+    // }
   });
 });
---
+++ src/weather-interface.js
@@ -0,0 +1,1 @@
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`
