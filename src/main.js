8c1321334b8327940129a68de6f3fa8f8f3ef5e8
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-29 13:50:04 -0700

    update weatherApi app to tuesday lesson
    Co-authored-by: Erika Debelis <erika.debelis@gmail.com>
    Co-Authored-By: Godfrey Owidi<godfreyowiidi@gmail.com>

--- src/main.js
+++ src/main.js
@@ -22,38 +22,63 @@ $(document).ready(function() {
         
       } else if (inputtedParameter === "q") {
         return`http://api.openweathermap.org/data/2.5/weather?${inputtedParameter}=${choice}&appid=${process.env.API_KEY}`;
-         
+    
       } else if (inputtedParameter === "zip") {
         return `http://api.openweathermap.org/data/2.5/weather?${inputtedParameter}=${choice}&appid=${process.env.API_KEY}`;
         
       } else {
-        return "Try again!"
+        return "Try again!";
       }
   
     }
     console.log(result());
-    
-
-
-    //
-
-    //let request = new XMLHttpRequest();
- 
-    // request.onreadystatechange = function() {
-    //   if (this.readyState === 4 && this.status === 200) {
-    //     const response = JSON.parse(this.responseText);
-    //     getElements(response);
-    //   }
-    // };
-
-    // request.open("GET", url, true);
-    // request.send();
-
-    // function getElements(response) {
-    //   $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-    //   $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    //   $('.showPressure').text(`The pressure is ${response.main.pressure} inHg²`);
-    //   $('.showClouds').text(`The cloud coverage is: ${response.weather[0].description}`);
-    // }
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = result();
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });  
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+      $('.showPressure').text(`The pressure is ${body.main.pressure} inHg²`);
+      $('.showClouds').text(`The cloud coverage is: ${body.weather[0].description}`)
+      $('.showErrors').text("");
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+      $('showHumidity').text("");
+      $('.showTemp').text("");
+      $('.showPressure').text("");
+      $('.showClouds').text("");
+    });
   });
 });
+
+/* eslint-disable no-debugger */
+//let request = new XMLHttpRequest();
+
+// request.onreadystatechange = function() {
+//   if (this.readyState === 4 && this.status === 200) {
+//     const response = JSON.parse(this.responseText);
+//     getElements(response);
+//   }
+// };
+
+// request.open("GET", url, true);
+// request.send();
+
+// function getElements(response) {
+//   $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//   $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+//   $('.showPressure').text(`The pressure is ${response.main.pressure} inHg²`);
+//   $('.showClouds').text(`The cloud coverage is: ${response.weather[0].description}`);
+// }
+/* eslint-enable no-debugger */
--- src/weather-interface.js
+++ src/weather-interface.js
@@ -1,1 +1,5 @@
 const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`
+
+export const kelToFahr = (kelvins) => {
+  return Math.round((kelvins - 273.15) * 9/5 + 32)
+}
